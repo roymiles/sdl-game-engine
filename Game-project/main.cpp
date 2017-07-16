03b5e7ffab2bfa860fd13f1d846824ebcdaee7a3
Roy Miles <roy.miles12@gmail.com>
2017-07-16 20:59:57 +0100

    Sorting out my workspace

--- Game-project/main.cpp
+++ Game-project/main.cpp
@@ -3,7 +3,7 @@
 //  GameProject
 //
 //  Created by Aatina Punjabi on 16/07/2017.
-//  Copyright Â© 2017 Aatina Punjabi. All rights reserved.
+//  Copyright © 2017 Aatina Punjabi. All rights reserved.
 //
 
 #include <iostream>
@@ -13,37 +13,37 @@
 const int WIDTH = 800, HEIGHT = 600;
 
 int main(int argc, char * argv[]) {
-    
-    //Initialises SDL
-    if ( SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-        std::cout << "SDL could not initialise! SDL Error: " << SDL_GetError() << std::endl;
-    }
-    
-    //The window to be rendered into
-    SDL_Window *window = SDL_CreateWindow("Hello World!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI );
-    
-    //Return error if window can't be created
-    if ( NULL == window ) {
-        std::cout << "Could not create window: " << SDL_GetError() << std::endl;
-        
-        return EXIT_FAILURE;
-    }
-    
-    //Handles any event that occurs in app
-    SDL_Event windowEvent;
-    
-    //Constantly running window
-    while (true) {
-        if ( SDL_PollEvent( &windowEvent ) ) {
-            if ( SDL_QUIT == windowEvent.type ){
-                break;
-            }
-        }
-    }
-    
-    //Close the window
-    SDL_DestroyWindow( window );
-    SDL_Quit();
-    
-    return EXIT_SUCCESS;
+
+	//Initialises SDL
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+		std::cout << "SDL could not initialise! SDL Error: " << SDL_GetError() << std::endl;
+	}
+
+	//The window to be rendered into
+	SDL_Window *window = SDL_CreateWindow("Hello World!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+
+	//Return error if window can't be created
+	if (NULL == window) {
+		std::cout << "Could not create window: " << SDL_GetError() << std::endl;
+
+		return EXIT_FAILURE;
+	}
+
+	//Handles any event that occurs in app
+	SDL_Event windowEvent;
+
+	//Constantly running window
+	while (true) {
+		if (SDL_PollEvent(&windowEvent)) {
+			if (SDL_QUIT == windowEvent.type) {
+				break;
+			}
+		}
+	}
+
+	//Close the window
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return EXIT_SUCCESS;
 }
